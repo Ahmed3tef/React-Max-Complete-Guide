c00528905c0f60c1ea11da452e5599ca26b2b699
Ahmed Atef <66911947+Ahmed3tef@users.noreply.github.com>
2021-09-24 02:56:26 +0200

    Add files via upload

---
+++ section 10 (UseEffect & UseReducer & context API)/1- useEffect/src/App.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+
+import Login from './components/Login/Login';
+import Home from './components/Home/Home';
+import MainHeader from './components/MainHeader/MainHeader';
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const loggedInInfo = localStorage.getItem('isLoggedIn');
+    if (loggedInInfo === '1') setIsLoggedIn(true);
+  }, []);
+
+  const loginHandler = (email, password) => {
+    // We should of course check email and password
+    localStorage.setItem('isLoggedIn', '1');
+    // But it's just a dummy/ demo anyways
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <React.Fragment>
+      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
+      </main>
+    </React.Fragment>
+  );
+}
+
+export default App;
