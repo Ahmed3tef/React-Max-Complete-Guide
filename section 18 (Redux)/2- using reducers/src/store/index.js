e1941b84769706886bbb53e582d55b755384d7f7
Ahmed Atef <66911947+Ahmed3tef@users.noreply.github.com>
2021-10-03 17:17:40 +0200

    Add files via upload

---
+++ section 18 (Redux)/2- using reducers/src/store/index.js
@@ -0,0 +1,28 @@
+import { createStore } from 'redux';
+
+const intialState = { counter: 0, showCounter: true };
+
+const counterReducer = (state = intialState, action) => {
+  if (action.type === 'increment') {
+    return { counter: state.counter + 1, showCounter: state.showCounter };
+  }
+  if (action.type === 'increase') {
+    return {
+      counter: state.counter + action.amount,
+      showCounter: state.showCounter,
+    };
+  }
+  if (action.type === 'toggle') {
+    return { counter: state.counter, showCounter: !state.showCounter };
+  }
+
+  if (action.type === 'decrement') {
+    return { counter: state.counter - 1, showCounter: state.showCounter };
+  }
+
+  return { counter: state.counter, showCounter: state.showCounter };
+};
+
+const store = createStore(counterReducer);
+
+export default store;
